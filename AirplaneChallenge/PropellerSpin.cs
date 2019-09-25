bd11ee0db8c970a1f39ab383c000f7540f6c7112
ghalls99 <54293763+ghalls99@users.noreply.github.com>
2019-09-25 16:21:29 -0600

    Add files via upload

---
+++ AirplaneChallenge/PropellerSpin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public float propeller = 5.0f;
+    public float rotationSpeed = 600.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0,20) * rotationSpeed * Time.deltaTime);
+
+    }
+}
