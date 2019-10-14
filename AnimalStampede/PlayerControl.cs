93feb2a5983e89b027ad5437108724d966765f0a
ghalls99 <54293763+ghalls99@users.noreply.github.com>
2019-10-13 20:26:09 -0600

    Add files via upload

---
+++ AnimalStampede/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float leftRange = 20;
+    public float rightRange = 15;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -leftRange)
+        {
+            transform.position = new Vector3(-leftRange, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > rightRange)
+        {
+            transform.position = new Vector3(rightRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
