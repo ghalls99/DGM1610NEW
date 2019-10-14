56ef782005fd8f9df0d98cdf3c84ca5331653450
garrett <garrett@hallsnet.com>
2019-10-13 21:53:50 -0600

    Dog Game
    Created dog game including bonus challenges

---
+++ Dog/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogCooldown = .5f;
+    public float dogTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        dogTimer += Time.deltaTime;
+        if(dogTimer < dogCooldown)
+        {
+            return;
+        }
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dogTimer = 0;
+
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
