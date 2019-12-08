8e2140262247fe2ae20d0334eb51521dd2eabe1f
ghalls99 <54293763+ghalls99@users.noreply.github.com>
2019-12-07 17:49:13 -0700

    Add files via upload

---
+++ Lab4/SpawnManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnmanager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public gameObject powerup;
+
+    private float zEnemySpawn = 12.0f;
+    private float xSpawnRange = 16.0f;
+    private float zPowerupRange = 5.0f;
+    private float ySpawn = 0.75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+        SpawnPowerup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
+
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex], gameObject.transform.rotation);
+    }
+
+    void SpawnPowerup()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, randomZ);
+        Instantiate(powerup, spawnPos, powerup.gameObject.transform.rotation);
+    }
+
+}using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
